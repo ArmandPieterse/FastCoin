e96501fec23f8682673a0bd3b82859da96e8eb62
Hakujin <Hakujin@HAKUJIN-PC>
2016-07-21 18:14:07 +0200

    Add project files.

---
+++ FastCoinTrader/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FastCoinTrader.MvcApplication" Language="C#" %>
---
+++ FastCoinTrader/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FastCoinTrader.Startup))]
+namespace FastCoinTrader
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
