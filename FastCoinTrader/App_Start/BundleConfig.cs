dc81834ef048e5a01e8fbb7e5ca49d7c20cee82c
Fako <aashiqdurga@gmail.com>
2016-07-23 02:17:12 +0200

    #1 Added angularjs from CDN. Set up basic template for modification

--- FastCoinTrader/App_Start/BundleConfig.cs
+++ FastCoinTrader/App_Start/BundleConfig.cs
@@ -22,10 +22,11 @@ namespace FastCoinTrader
             bundles.Add(new ScriptBundle("~/bundles/bootstrap")
                 .Include("~/Scripts/bootstrap.js","~/Scripts/respond.js")
                 .IncludeDirectory("~/Scripts/BootstrapScripts", "*.js"));
-            
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css")
+            bundles.Add(new StyleBundle("~/Content/css").IncludeDirectory("~/Content", "*.css"));
 
 
         }
