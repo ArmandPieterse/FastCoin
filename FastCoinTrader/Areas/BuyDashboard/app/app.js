c87844aaec82779860f54edc0c856e9f48e13594
Fako <aashiqdurga@gmail.com>
2016-07-23 15:03:42 +0200

    Added angular infrastructure

---
+++ FastCoinTrader/Areas/BuyDashboard/app/app.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    "use strict";
+    var app = angular.module("buydashboard", ["ngRoute", "ngSanitize"]);
+    
+    // Directives
+    //app.directive('countries', countriesDropdownDirective);
+    //app.directive('provinces', provincesDropdownDirective);
+
+    // Factories
+    //app.factory("buyDashboardModel", buyDashboardModel);
+
+    //// Services
+    //app.service("apiService", apiService);
+    //app.service("navigationService", navigationService);
+    //app.service("buyDashboardService", buyDashboardService);
+
+    app.config(function ($routeProvider) {
+        $routeProvider.when("/home",
+            {
+                çontroller: "buyDashboardHomeController",
+                templateUrl: "/Areas/BuyDashboard/app/templates/buyDashboardHomeView.html"
+            }).otherwise({ redirectTo: "/home" });
+    });
+})();
