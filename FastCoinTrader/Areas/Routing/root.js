97a81d03ea295cac9bd1c246479c580695741dbc
Arno <bornmana101@gmail.com>
2016-08-15 09:18:10 +0200

    prep for routing fix

---
+++ FastCoinTrader/Areas/Routing/root.js
@@ -0,0 +1,40 @@
+﻿/// <reference path="C:\Users\Arno\Documents\Visual Studio 2015\Projects\Personal\FastCoinTest\FastCoinTrader\Views/Home/Index.cshtml" />
+/// <reference path="C:\Users\Arno\Documents\Visual Studio 2015\Projects\Personal\FastCoinTest\FastCoinTrader\Views/Home/Index.cshtml" />
+(function () {
+    "use strict";
+    angular.module("root", ["ngRoute", "ngSanitize", "ngiUI", "ui.bootstrap", "smart-table", "buydashboard", "selldashboard", "tradeHistory"])
+
+    .controller("myApp",[function() {
+            go = function() {
+                alert("Success");
+            }
+
+            go();
+        }])
+
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when("/home",
+                {  
+                    templateUrl: "/Views/Home/Index"
+                })
+                .when("/test",
+                {
+                    templateUrl: "/Areas/BuyDashboard/app/templates/myTest.html"
+                })
+            .when("/buy",
+                {
+                    templateUrl: "/Areas/BuyDashboard/app/templates/buyDashBoardHomeView.html"
+                })
+            .when("/sell",
+                {
+                    templateUrl: "/Areas/SellDashboard/app/templates/sellDashBoardHomeView.html"
+                })
+            .when("trade",
+                {
+                    templateUrl: "/Areas/SellDashboard/app/templates/sellDashBoardHomeView.html"
+                });
+
+            //.otherwise({ redirectTo: "/home" });
+        });
+})();
