51b98771490470cbe3989470d6dd70f1f3f907c2
armandpieterse <armandpie@gmail.com>
2016-08-20 21:50:23 +0200

    News helper and controller

--- FastCoinTrader/Controllers/NewsController.cs
+++ FastCoinTrader/Controllers/NewsController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FastCoinTrader.EnitityModels.EntityHelper;
+using Newtonsoft.Json;
+
 
 namespace FastCoinTrader.Controllers
 {
@@ -12,6 +15,14 @@ namespace FastCoinTrader.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult SaveNewsEntry(string title,string paragraph,string videoLink)
+        {
+            string message = NewsEntityHelper.CreateNewsEntry(title, paragraph,videoLink);
+            return Json(new { Message = ""});
+
         }
     }
 }
