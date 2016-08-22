9945899ca7b07bb76bf9fdbfa44f4dd9cf659a5b
Armand Pieterse <armandpie@gmail.com>
2016-08-22 10:36:03 +0200

    FAQ entity helper and controller

--- FastCoinTrader/Controllers/FAQController.cs
+++ FastCoinTrader/Controllers/FAQController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FastCoinTrader.EnitityModels;
+using FastCoinTrader.EnitityModels.EntityHelper;
+
 
 namespace FastCoinTrader.Controllers
 {
@@ -12,6 +15,13 @@ namespace FastCoinTrader.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult SaveFAQEntry(string title, string content, string panelNumber)
+        {
+            string message =  FAQEntityHelper.CreateFAQEntry(title, content, int.Parse(panelNumber));
+            return Json(new { Message = message });
+            //return Json("");
         }
     }
 }
